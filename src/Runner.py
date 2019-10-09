6504f439110fc84b03574c2e1d385d10d72d755e
gabvaztor <gabvaztor@gmail.com>
2019-10-09 09:27:32 +0200

    Adding default decorators with print message

--- src/Runner.py
+++ src/Runner.py
@@ -1,15 +1,17 @@
 import Config.GlobalDecorators as gd
 
 def msg_printer(**kwargs):
-    """
-    Args:
-        var:
-        *args:
-        **kwargs:
-    """
-    print("ads ->" , kwargs["DEBUG"])
+    print("debug ->" , kwargs["DEBUG"])
+
+
+def msg_printer2():
+    print("debug ->" , )
+
 import sys
 modname = globals()['__name__']
+print(modname)
+print(gd.__name__)
 module = sys.modules
 gd.DecoratorClass().start_wrapper_decoration(module[modname])
 msg_printer()  # prints 'Message'
+msg_printer2()
